89c890473a97fa637db736eb0b978c0d682c0c20
Gregory Szorc <gregory.szorc@gmail.com>
2022-07-01 13:36:37 -0700

    binary-portability: start to implement ELF object file parsing
    Doesn't have nearly all the functionality we require. But you have
    to start somewhere.

---
+++ binary-portability/src/elf.rs
@@ -0,0 +1,89 @@
+// Copyright 2022 Gregory Szorc.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use {
+    crate::{ObjectFileInfo, Result, UndefinedSymbol},
+    object::{
+        elf::{DT_NEEDED, SHT_DYNSYM},
+        read::elf::{Dyn, FileHeader, SectionHeader, Sym},
+        Endianness, SectionIndex,
+    },
+};
+
+pub fn analyze_elf<Elf: FileHeader<Endian = Endianness>>(data: &[u8]) -> Result<ObjectFileInfo> {
+    let in_elf = Elf::parse(data)?;
+    let endian = in_elf.endian()?;
+
+    let sections = in_elf.sections(endian, data)?;
+
+    let symbol_versions = sections.versions(endian, data)?;
+
+    let mut dt_needed = vec![];
+    let mut undefined = vec![];
+
+    for (section_index, section) in sections.iter().enumerate() {
+        if let Some((entries, strings_index)) = section.dynamic(endian, data)? {
+            let strings = sections.strings(endian, data, strings_index)?;
+
+            for entry in entries {
+                // DT_NEEDED defines external libraries we require.
+                if entry.tag32(endian) == Some(DT_NEEDED) {
+                    let value = entry.string(endian, strings)?;
+                    let value = String::from_utf8_lossy(value).to_string();
+
+                    dt_needed.push(value);
+                }
+            }
+        }
+
+        if let Some(symbols) =
+            section.symbols(endian, data, &sections, SectionIndex(section_index))?
+        {
+            let strings = symbols.strings();
+
+            for (symbol_index, symbol) in symbols.iter().enumerate() {
+                let name = symbol.name(endian, strings)?;
+                let name = String::from_utf8_lossy(name).to_string();
+
+                // If we're in the .dynsym section, there should be version info for
+                // every symbol.
+                let symbol_version = if section.sh_type(endian) == SHT_DYNSYM {
+                    if let Some(versions) = &symbol_versions {
+                        let version_index = versions.version_index(endian, symbol_index);
+
+                        if let Some(version) = versions.version(version_index)? {
+                            let version = version.name();
+                            let version = String::from_utf8_lossy(version).to_string();
+
+                            Some(version)
+                        } else {
+                            None
+                        }
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                };
+
+                if symbol.is_undefined(endian) {
+                    undefined.push(UndefinedSymbol {
+                        name,
+                        version: symbol_version,
+                        library: None,
+                    });
+                }
+            }
+        }
+    }
+
+    Ok(ObjectFileInfo {
+        required_libraries: dt_needed,
+        undefined_symbols: undefined,
+    })
+}
--- binary-portability/src/lib.rs
+++ binary-portability/src/lib.rs
@@ -8,4 +8,38 @@
 
 //! Inspect binaries for portability concerns.
 
+pub mod elf;
 pub mod linux_standard_base;
+
+use thiserror::Error;
+
+/// Crate's error enumeration.
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("object file error{0}")]
+    Object(#[from] object::Error),
+}
+
+/// Crate's result type.
+pub type Result<T> = std::result::Result<T, Error>;
+
+/// An object file symbol that is not defined.
+pub struct UndefinedSymbol {
+    /// The symbol's name.
+    pub name: String,
+
+    /// Symbol version.
+    pub version: Option<String>,
+
+    /// Library the symbol is in.
+    pub library: Option<String>,
+}
+
+/// Information from a parsed object file.
+pub struct ObjectFileInfo {
+    /// Libraries that this object file references.
+    pub required_libraries: Vec<String>,
+
+    /// Undefined symbols.
+    pub undefined_symbols: Vec<UndefinedSymbol>,
+}
