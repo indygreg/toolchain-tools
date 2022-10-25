975e828671d2c9ff4f1f2535c7165e5a42ab3dea
Gregory Szorc <gregory.szorc@gmail.com>
2022-10-24 19:45:48 -0700

    toolchain-bootstrap: upgrade LLVM 15.0.0 -> 15.0.3

--- toolchain-bootstrap/scripts/clang-macos.py
+++ toolchain-bootstrap/scripts/clang-macos.py
@@ -43,9 +43,9 @@ DOWNLOADS = [
     },
     {
         "name": "llvm",
-        "url": "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.0/llvm-project-15.0.0.src.tar.xz",
-        "sha256": "caaf8100365b6ebafc39fea803e902ca3ff38b4d5327b9927097808d32964db7",
-        "version": "15.0.0",
+        "url": "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.3/llvm-project-15.0.3.src.tar.xz",
+        "sha256": "dd07bdab557866344d85ae21bbeca5259d37b4b0e2ebf6e0481f42d1ba0fee88",
+        "version": "15.0.3",
     }
 ]
 
