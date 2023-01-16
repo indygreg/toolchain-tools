0402e882cd3c8944fa5a37687c2e830e9c96e1e4
Gregory Szorc <gregory.szorc@gmail.com>
2023-01-15 19:28:54 -0800

    toolchain-bootstrap: add building of standalone bolt binaries
    I want to start using BOLT. But the version in LLVM 15.0.7 has a
    handful of bugs that I'm hitting.
    
    This commit introduces a build of BOLT from latest Git commit for
    Linux x86-64. Binaries should be standalone and runnable on any
    reasonably modern Linux distro.

---
+++ toolchain-bootstrap/scripts/bolt.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -x
+
+export PATH=/toolchain/bin:$PATH
+echo /toolchain/lib > /etc/ld.so.conf.d/toolchain
+
+tar -C /usr/local --strip-components=1 -xf /build/cmake.tar.gz
+unzip ninja.zip
+mv ninja /usr/local/bin/
+
+pushd llvm-project
+git checkout "${LLVM_COMMIT}"
+popd
+
+mkdir build
+pushd build
+cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_C_COMPILER_LAUNCHER=sccache \
+  -DCMAKE_CXX_COMPILER_LAUNCHER=sccache \
+  -DCMAKE_EXE_LINKER_FLAGS="-static-libstdc++" \
+  -DCMAKE_INSTALL_PREFIX=/toolchain \
+  -DLLVM_TARGETS_TO_BUILD="X86;AArch64" \
+  -DLLVM_ENABLE_ASSERTIONS=ON \
+  -DLLVM_ENABLE_PROJECTS="bolt" \
+  -G Ninja \
+  ../llvm-project/llvm
+
+LD_LIBRARY_PATH=/toolchain/lib DESTDIR=/build/out ninja -j ${PARALLEL_NINJA} -l ${NINJA_MAX_LOAD} bolt
+
+mkdir -p /build/out/toolchain/bin
+cp -av bin/{llvm-bolt,llvm-boltdiff,merge-fdata,perf2bolt} /build/out/toolchain/bin/
+
+popd
+
+# Free up a few gigabytes.
+rm -rf llvm.git build
+
+sccache -s
