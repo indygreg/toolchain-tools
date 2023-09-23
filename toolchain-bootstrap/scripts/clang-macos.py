593ad5adb952b77d6d18ee511c14d0a4da19ccfb
Gregory Szorc <gregory.szorc@gmail.com>
2023-09-22 22:54:10 -0700

    toolchain-bootstrap: llvm 16.0.6 -> 17.0.1
    This seemed too easy.

--- toolchain-bootstrap/scripts/clang-macos.py
+++ toolchain-bootstrap/scripts/clang-macos.py
@@ -43,9 +43,9 @@ DOWNLOADS = [
     },
     {
         "name": "llvm",
-        "url": "https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.6/llvm-project-16.0.6.src.tar.xz",
-        "sha256": "ce5e71081d17ce9e86d7cbcfa28c4b04b9300f8fb7e78422b1feb6bc52c3028e",
-        "version": "16.0.6",
+        "url": "https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.1/llvm-project-17.0.1.src.tar.xz",
+        "sha256": "b0e42aafc01ece2ca2b42e3526f54bebc4b1f1dc8de6e34f46a0446a13e882b9",
+        "version": "17.0.1",
     },
 ]
 
