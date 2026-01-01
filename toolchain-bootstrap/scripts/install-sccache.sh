7979e932de4a45d698355864eb8730c4680db255
Gregory Szorc <gregory.szorc@gmail.com>
2026-01-01 10:47:21 -0800

    toolchain-bootstrap: sccache 0.10.0 -> 0.12.0

--- toolchain-bootstrap/scripts/install-sccache.sh
+++ toolchain-bootstrap/scripts/install-sccache.sh
@@ -4,14 +4,14 @@ set -euo errexit -o pipefail
 
 if [ $(uname -m) = "x86_64" ]; then
   arch=x86_64
-  sha256=1fbb35e135660d04a2d5e42b59c7874d39b3deb17de56330b25b713ec59f849b
+  sha256=b0e89ead6899224a4ba2b90e9073bf1ce036d95bab30f3dc33c1e1468bc4ad44
 else
   arch=aarch64
-  sha256=d6a1ce4acd02b937cd61bc675a8be029a60f7bc167594c33d75732bbc0a07400
+  sha256=111ddd28fb108cb3e17edb69ab62cefe1dcc97b02e5006ff9c1330f4f2e78368
 fi
 
 secure-download.sh \
-  https://github.com/mozilla/sccache/releases/download/v0.10.0/sccache-v0.10.0-${arch}-unknown-linux-musl.tar.gz \
+  https://github.com/mozilla/sccache/releases/download/v0.12.0/sccache-v0.12.0-${arch}-unknown-linux-musl.tar.gz \
   ${sha256} \
   sccache.tar.gz
 
